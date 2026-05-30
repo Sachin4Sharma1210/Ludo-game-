f1f5e76222a45a1445edf0a4044225d2b74fdfd9
Sachin4Sharma1210 <rajbala281210@gmail.com>
2026-05-30 08:26:01 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ये वैल्यूज वर्सेल (Vercel) की सेटिंग्स से आएंगी
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
